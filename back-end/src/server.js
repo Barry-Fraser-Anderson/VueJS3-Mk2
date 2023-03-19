538ada373eeebff3e632ea3f813f9c7a367e6106
Barry Anderson <barryanderson60@outlook.com>
2023-03-19 21:53:43 +0000

    Created server

---
+++ back-end/src/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import { cartItems, products } from './temp-data.js';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello!');
+});
+
+app.get('/products', (req, res) => {
+  res.json(products);
+});
+
+app.get('/cart', (req, res) => {
+  res.json(cartItems);
+});
+
+app.get('/products/:productId', (req, res) => {
+  const productId = req.params.productId;
+  const product = products.find(product => product.id === productId);
+  res.json(product);
+});
+
+app.listen(8000, () => {
+  console.log('Server is listening on port 8000');
+});
