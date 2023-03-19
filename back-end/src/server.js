e738eff469097631e19964518cb48b211fa37ae5
Barry Anderson <barryanderson60@outlook.com>
2023-03-19 22:15:21 +0000

    Add and delete server data

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,7 +1,11 @@
 import express from 'express';
-import { cartItems, products } from './temp-data.js';
+import { cartItems as cartItemsRsw, products as productsRaw } from './temp-data.js';
+
+let cartItems = cartItemsRsw;
+let products = productsRaw;
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello!');
@@ -18,7 +22,20 @@ app.get('/cart', (req, res) => {
 app.get('/products/:productId', (req, res) => {
   const productId = req.params.productId;
   const product = products.find(product => product.id === productId);
+  cartItems.push(product);
+  res.json(cartItems);
+});
+
+app.post('/cart', (req, res) => {
+  const productId = req.body.id;
+  const product = products.find(product => product.id === productId);
   res.json(product);
+});
+
+app.delete('/cart/:productId', (req, res) => {
+  const productId = req.params.productId;
+  const cartItems = cartItems.filter(product => product.id !== productId);
+  res.json(cartItems);
 });
 
 app.listen(8000, () => {
