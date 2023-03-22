4aa98680faa19379858b7d264f1d3b88278d93fc
Barry Anderson <barryanderson60@outlook.com>
2023-03-22 22:35:48 +0000

    Load data via the back-end

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import { MongoClient } from 'mongodb';
+import path from 'path';
 
 async function start() {
   const DATABASE = 'mongodb+srv://barryanderson:<PASSWORD>@cluster0.iiobiwd.mongodb.net/vue-db';
-  const URL = DATABASE.replace('<PASSWORD>', 'xxxxxxxxxxx');
-
+  const URL = DATABASE.replace('<PASSWORD>', '471WxFVa97n4QHOU');
   const client = new MongoClient(URL);
   await client.connect();
   const db = client.db('vue-db');
@@ -12,7 +12,9 @@ async function start() {
   const app = express();
   app.use(express.json());
 
-  app.get('/products', async (req, res) => {
+  app.use('/images', express.static(path.join(__dirname, '../assets')));
+
+  app.get('/api/products', async (req, res) => {
     const products = await db.collection('products').find({}).toArray();
     res.send(products);
   });
@@ -21,19 +23,19 @@ async function start() {
     return Promise.all(ids.map(id => db.collection('products').findOne({ id })));
   }
 
-  app.get('/users/:userId/cart', async (req, res) => {
+  app.get('/api/users/:userId/cart', async (req, res) => {
     const user = await db.collection('users').findOne({ id: req.params.userId });
     const populatedCart = await populateCartIds(user.cartItems);
     res.json(populatedCart);
   });
 
-  app.get('/products/:productId', async (req, res) => {
+  app.get('/api/products/:productId', async (req, res) => {
     const productId = req.params.productId;
     const product = await db.collection('products').findOne({ id: productId });
     res.json(product);
   });
 
-  app.post('/users/:userId/cart', async (req, res) => {
+  app.post('/api/users/:userId/cart', async (req, res) => {
     const userId = req.params.userId;
     const productId = req.body.id;
 
@@ -49,7 +51,7 @@ async function start() {
     res.json(populatedCart);
   });
 
-  app.delete('/users/:userId/cart/:productId', async (req, res) => {
+  app.delete('/api/users/:userId/cart/:productId', async (req, res) => {
     const userId = req.params.userId;
     const productId = req.params.productId;
 
